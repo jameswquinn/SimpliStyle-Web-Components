70bc6355c9b320f5f133f4d74c8b94f3c2ad6aa2
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-19 19:17:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,32 @@
-placeholder
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/simplistyle-components.js'),
+      name: 'SimpliStyle',
+      fileName: (format) => `simplistyle.${format}.js`
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
+    }
+  },
+  plugins: [
+    {
+      name: 'custom-css-plugin',
+      generateBundle(options, bundle) {
+        bundle['simplistyle-global.css'] = {
+          isAsset: true,
+          type: 'asset',
+          fileName: 'simplistyle-global.css',
+          source: require('fs').readFileSync(resolve(__dirname, 'src/simplistyle-global.css'), 'utf-8')
+        };
+      }
+    }
+  ]
+});
